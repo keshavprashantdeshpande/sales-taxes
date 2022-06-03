651a2c40191a601fa449de9f89a812df5cfe1ef5
11deke1mst <11deke1mst@hft-stuttgart.de>
2022-06-03 22:25:31 +0200

    Initialize spring boot project

---
+++ src/test/java/com/codingchallenge/salestax/SalestaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingchallenge.salestax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalestaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
